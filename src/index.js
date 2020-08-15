fcfba45caeec50c4c28249d399cac0b63572cf2a
arihant557 <5arihant.jain5@gmail.com>
2020-08-15 17:32:01 +0530

    created elements and added properties

--- src/index.js
+++ src/index.js
@@ -3,16 +3,17 @@ import ReactDOM from 'react-dom';
 import './index.css';
 // import * as serviceWorker from './serviceWorker';
 
-
+//git remote add origin https://github.com/arihant557/react.git
 
 ReactDOM.render
 (
+   //created a fxn that will render i.e. createElement which has 3 arguments a) name of tag   b) any property   c) children or text
 
-  React.createElement("h1" , null , "Working  on  it"),
-  document.getElementById('root')
+  React.createElement("h1" , {style:{color : "red"} } , "Working  on  it "),
+  document.getElementById('root'),
+  console.log("Hi")
 
 );
-
 
 
 
