85cce12b8b61d02d93be38ad712fd603624876c9
arihant557 <5arihant.jain5@gmail.com>
2020-08-15 18:35:06 +0530

    jsx usage

--- src/index.js
+++ src/index.js
@@ -7,20 +7,40 @@ import './index.css';
 
 ReactDOM.render
 (
+
+
+
           //     1
          //created a fxn that will render i.e. createElement which has 3 arguments a) name of tag   b) any property (by passing in object)  c) children or text
         //React.createElement("h1" , {style:{color : "red"} } , "Working  on  it ..."),
+
+
 
 
     //     2
    //rendering multiple elements just by makingg changes in children
   //React.createElement("h1" , {style:{color : "red"} } , React.createElement("p" , {style:{color : "green"} } , "progress")),
   
-       //    3     making list
-      React.createElement("ul" , {style:{color : "red"} } , 
-      React.createElement("li" , null , "english"),
-      React.createElement("li" , null , "maths"),
-      React.createElement("li" , null , "science")),
+
+
+
+
+        //    3     making list
+       // React.createElement("ul" , {style:{color : "red"} } , 
+      // React.createElement("li" , null , "english"),
+      // React.createElement("li" , null , "maths"),
+      // React.createElement("li" , null , "science")),
+
+
+
+
+      //////   jsx   ///// no need of elements and all babel is handling whole that thing
+
+      <ul>
+        <li>burger</li>
+        <li>pizza</li>
+        <li>fries</li>
+      </ul>,
   
   
   
@@ -28,6 +48,25 @@ ReactDOM.render
   console.log("Hi")
 
 );
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
